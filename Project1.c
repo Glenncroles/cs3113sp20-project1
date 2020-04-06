7d8c3bca26d9f20d6883a81e5308f977493a9a39
Glenncroles <43144720+Glenncroles@users.noreply.github.com>
2020-04-06 18:43:43 -0500

    Update Project1.c
    oops

--- Project1.c
+++ Project1.c
@@ -283,7 +283,7 @@ int main(int argc, char** argv)
 	struct Link* head = NULL;
 
 	//bringing in the file this is for testing
-	FILE* input = fopen("OStest1.txt", "r");
+	FILE* input = fopen(argv[1], "r");
 
 	/**
 	 * reading in the file
